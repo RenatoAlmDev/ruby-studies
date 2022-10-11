c6c46cc836a00bdc2b3eb97363aa91228ce140fa
Renato Almeida <almeida.renato1988@gmail.com>
2022-10-11 09:39:23 -0300

    primeiras aulas de Ruby puro

---
+++ entrada_saida.rb
@@ -0,0 +1,9 @@
+# Saída de dado:
+print "digite seu nome: "
+
+# Recebendo a entrada do teclado:
+name = gets.chomp
+
+# Saída utilizando puts
+# use códigos ruby dentro de uma string com #{code}
+puts "Hello #{name}"
---
+++ times.rb
@@ -0,0 +1,5 @@
+10.times do
+  puts "Oi"
+end
+
+# Coloca o valor inteiro e executa o ".times", ele vai executar o bloco o número de vezes digitado
